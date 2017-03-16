d2d689e913d5f695ba6c8d951ee582ddf70730e2
James Parker <jamez226@hotmail.com>
2017-03-16 20:47:05 +0000

    Add travis build

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/user/bin/env bash
+dotnet restore dockertest.sln
+dotnet build -c Release dockertest/dockertest.csproj
+dotnet publish -c Release -o obj/Docker/publish dockertest/dockertest.csproj
+docker build -t james226/dockertest:$TRAVIS_BUILD_NUMBER
