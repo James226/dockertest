1eb21dc51bd1151cc6c8efc36b95dcac099d98d3
James Parker <jamez226@hotmail.com>
2017-03-16 21:11:29 +0000

    Add temp linux dockerfile

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 dotnet restore dockertest.sln
 dotnet build -c Release dockertest/dockertest.csproj
 dotnet publish -c Release -o obj/Docker/publish dockertest/dockertest.csproj
-docker build -t james226/dockertest:$TRAVIS_BUILD_NUMBER dockertest
+docker build -t james226/dockertest:$TRAVIS_BUILD_NUMBER dockertest -f dockertest/Dockerfile.linux
