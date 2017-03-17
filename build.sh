873d9186b04b7bc16629d37396665ff87f9bec31
James <jamez226@hotmail.com>
2017-03-17 07:47:32 +0000

    Push docker image to AWS via Travis

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+pip install --upgrade awscli
 dotnet restore dockertest.sln
 dotnet build -c Release dockertest/dockertest.csproj
 dotnet publish -c Release -o obj/Docker/publish dockertest/dockertest.csproj
 docker build -t james226/dockertest:$TRAVIS_BUILD_NUMBER dockertest -f dockertest/Dockerfile.linux
+eval $(aws ecr get-login --region eu-west-1)
+docker tag james226/dockertest:$TRAVIS_BUILD_NUMBER 691267057566.dkr.ecr.eu-west-1.amazonaws.com/james226/dockertest:$TRAVIS_BUILD_NUMBER
+docker push 691267057566.dkr.ecr.eu-west-1.amazonaws.com/james226/dockertest:$TRAVIS_BUILD_NUMBER
