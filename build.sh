5bf8670d3ac92cb2275d0c183328059432651c09
James Parker <jamez226@hotmail.com>
2017-03-17 08:28:30 +0000

    Tag linux builds with -linux

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ pip install --upgrade awscli
 dotnet restore dockertest.sln
 dotnet build -c Release dockertest/dockertest.csproj
 dotnet publish -c Release -o obj/Docker/publish dockertest/dockertest.csproj
-docker build -t james226/dockertest:$TRAVIS_BUILD_NUMBER dockertest -f dockertest/Dockerfile.linux
+docker build -t james226/dockertest:latest dockertest -f dockertest/Dockerfile.linux
 eval $(aws ecr get-login --region eu-west-1)
-docker tag james226/dockertest:$TRAVIS_BUILD_NUMBER 691267057566.dkr.ecr.eu-west-1.amazonaws.com/james226/dockertest:$TRAVIS_BUILD_NUMBER
-docker push 691267057566.dkr.ecr.eu-west-1.amazonaws.com/james226/dockertest:$TRAVIS_BUILD_NUMBER
+docker tag james226/dockertest:latest 691267057566.dkr.ecr.eu-west-1.amazonaws.com/james226/dockertest:$TRAVIS_BUILD_NUMBER-linux
+docker push 691267057566.dkr.ecr.eu-west-1.amazonaws.com/james226/dockertest:$TRAVIS_BUILD_NUMBER-linux
