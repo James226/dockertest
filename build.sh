023865cd493fcdf891f47245d32e4482c4fca8cd
James Parker <jamez226@hotmail.com>
2017-04-06 12:06:33 +0100

    Change AWS Account

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ dotnet build -c Release dockertest/dockertest.csproj
 dotnet publish -c Release -o obj/Docker/publish dockertest/dockertest.csproj
 docker build -t james226/dockertest:latest dockertest -f dockertest/Dockerfile.linux
 eval $(aws ecr get-login --region eu-west-1)
-docker tag james226/dockertest:latest 691267057566.dkr.ecr.eu-west-1.amazonaws.com/james226/dockertest:$TRAVIS_BUILD_NUMBER-linux
-docker push 691267057566.dkr.ecr.eu-west-1.amazonaws.com/james226/dockertest:$TRAVIS_BUILD_NUMBER-linux
+docker tag james226/dockertest:latest 108652351904.dkr.ecr.eu-west-1.amazonaws.com/dockertest:$TRAVIS_BUILD_NUMBER-linux
+docker push 108652351904.dkr.ecr.eu-west-1.amazonaws.com/dockertest:$TRAVIS_BUILD_NUMBER-linux
